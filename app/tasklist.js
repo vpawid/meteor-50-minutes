a76fb4bc918d0e8e55ece419dd24dba9a8b73d62
Victor Pawid <niceguyhms@gmail.com>
2017-10-07 15:23:13 -1000

    Task 4: Tasklist authentication

--- app/tasklist.js
+++ app/tasklist.js
@@ -11,18 +11,15 @@ if (Meteor.isClient) {
     "submit .add-task": function(event){
       var name = event.target.name.value;
 
-      Tasks.insert({
-        name: name,
-        createdAt: new Date()
-      });
+      Meteor.call('addTask', name);
 
       event.target.name.value = '';
 
       return false;
     },
     "click .delete-task": function(event) {
-      if(confirm('Delet Task?')) {
-        Tasks.remove(this._id);
+      if(confirm('Delete Task?')) {
+        Meteor.call('deleteTask', this._id);
       }
       return false;
     }
@@ -33,3 +30,19 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
 
 }
+
+Meteor.methods({
+  addTask: function(name) {
+    if(!Meteor.userId()){
+      throw new Meteor.Error('No Access!');
+    }
+    Tasks.insert({
+      name: name,
+      createdAt: new Date(),
+      userId: Meteor.userId()
+  });
+ },
+  deleteTask: function(taskId){
+    Tasks.remove(taskId);
+  }
+});
